55db066de2a44db6a60934650ccc97213f847f33
Theo-Dorh <dorhtheophilus@gmail.com>
2025-02-21 15:25:39 +0000

    Created README files and shell script

---
+++ pushChanges.sh
@@ -0,0 +1,12 @@
+echo "Hello, we will now push your changes to the remote"
+read -p "Specify the files you'd like to push to the remote (type . to
+push all files): " fileName
+git add $fileName
+read -p "Please type your commit message: " commitMessage
+git commit -m "$commitMessage"
+git pull
+git push -u origin main
+
+Your changes have been successfully pushed to your remote repository.
+All the best!
+
